ccf2e9c54375386742680682fe4f0136402908f5
E. Gökçen Solmaz <gokcensolmaz99@gmail.com>
2023-12-07 22:01:33 +0300

    Click event handling

--- app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
+++ app/src/main/java/com/example/littlelemonlogin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.littlelemonlogin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -16,6 +17,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,68 +27,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainComponent()
+            UpperPanel()
         }
     }
 }
 
-@Composable
-fun MainComponent() {
-    Column(
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color(0Xff495e57))
-    ) {
-        Text(
-            text = stringResource(id = R.string.little_lemon),
-            fontSize = 32.sp,
-            color = Color(0xFFF4CE14),
-            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.chicago),
-            fontSize = 24.sp,
-            color = Color(0xFFFfffff),
-            modifier = Modifier.padding(start = 20.dp)
-        )
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(id = R.string.description),
-                Modifier.width(200.dp),
-                color = Color.White,
-                fontSize = 21.sp
-            )
-            Image(
-                painter = painterResource(id = R.drawable.little_lemon),
-                contentDescription = "",
-                Modifier
-                    .height(200.dp)
-                    .clip(RoundedCornerShape(20.dp)),
-            )
-        }
-        Button(
-            onClick = { },
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.padding(horizontal = 20.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xfff4ce14))
-        ) {
-            Text(text = stringResource(id = R.string.order))
-        }
 
-    }
-}
 
-@Preview
-@Composable
-fun MainComponentPreview() {
-    MainComponent()
-}
+
 
 
